8ffdbd731a518fe56368dfe3d0bf1eb3cdd925cb
Altus Janse Van Rensburg <albertusbjvr@gmail.com>
2021-09-11 22:16:10 -0300

    add explanation of collatz conjecture

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,21 @@ function App() {
 
   return (
     <div className="App">
-      <h1>X3N</h1>
+      <h1>Collatz conjecture</h1>
+      <p>
+        The{" "}
+        <strong>
+          <a href="https://en.wikipedia.org/wiki/Collatz_conjecture">Collatz conjecture</a>
+        </strong>{" "}
+        is a conjecture in mathematics that concerns sequences defined as follows: start with any
+        positive integer <strong>n</strong>. Then each term is obtained from the previous term as
+        follows: if the previous term is even, the next term is one half of the previous term. If
+        the previous term is odd, the next term is 3 times the previous term plus 1. The conjecture
+        is that no matter what value of <strong>n</strong>, the sequence will always reach 1.
+      </p>
+      <p>if (n % 2 === 0) = n / 2</p>
+      <p>if (n % 2 !== 0) = n * 3 + 1</p>
+      <p>if( n === 1) stop</p>
       <InputForm
         getSeedNum={(e) => setSeedNum(e.target.value)}
         seedSet={(e) => {
