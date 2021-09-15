958c15c65c3e906b85c3f5232b0d7810797e985e
Altus Janse Van Rensburg <albertusbjvr@gmail.com>
2021-09-15 00:19:43 -0300

    add boundary of seed is NaN

--- src/functions/x3n.js
+++ src/functions/x3n.js
@@ -1,23 +1,20 @@
 const x3n = async (x) => {
   let n = parseInt(x);
-  const startingNumber = parseInt(x);
-  if (n === isNaN) {
-    return "Not a number";
-  } else {
-    let calculatedArray = [startingNumber];
-    while (n !== 1) {
-      if (n % 2 !== 0) {
-        n = n * 3 + 1;
-        calculatedArray = [...calculatedArray, n];
-      }
-      if (n % 2 === 0) {
-        n = n / 2;
-        calculatedArray = [...calculatedArray, n];
-      }
+  let calculatedArray = [n];
+  while (n !== 1) {
+    if (typeof n !== "number") {
+      break;
     }
-
-    return calculatedArray;
+    if (n % 2 !== 0) {
+      n = n * 3 + 1;
+      calculatedArray = [...calculatedArray, n];
+    }
+    if (n % 2 === 0) {
+      n = n / 2;
+      calculatedArray = [...calculatedArray, n];
+    }
   }
+  return calculatedArray;
 };
 
 export default x3n;
