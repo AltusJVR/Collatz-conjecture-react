043934ddbca57e6dcef8c1cfe094eb74f5bd0bc7
Altus Janse Van Rensburg <albertusbjvr@gmail.com>
2021-09-29 10:38:01 -0300

    add hover value to each bar

--- src/Components/Chart/ChartBar.js
+++ src/Components/Chart/ChartBar.js
@@ -1,12 +1,16 @@
 const ChartBar = (props) => {
   const { max, value } = props;
 
-  let barValue = 0;
+  let height = 0;
   if (max > 0) {
-    barValue = (value / max) * 100 + "%";
+    height = (value / max) * 100 + "%";
   }
 
-  return <div className={props.className} style={{ height: barValue }}></div>;
+  return (
+    <div className={props.className} style={{ height: height }}>
+      <p>n: {value}</p>
+    </div>
+  );
 };
 
 export default ChartBar;
